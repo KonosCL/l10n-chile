4d97fe7b41945e00829a6736a49f93a6bb7bdade
Nelson Ramírez Sánchez <info@konos.cl>
2021-02-05 16:48:23 -0300

    [FIX] Typo

--- l10n_cl_etd_stock/models/stock_picking.py
+++ l10n_cl_etd_stock/models/stock_picking.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2019 CubicERP
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, models
+from odoo import api, models, fields
 
 
 class StockPicking(models.Model):
